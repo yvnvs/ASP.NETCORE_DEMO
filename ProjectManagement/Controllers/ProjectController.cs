d8900e93584673a8988244066775247c079e8ca4
Yunus <yunusinne@gmail.com>
2022-12-06 02:05:36 +0300

    Added: + Logging extension

--- ProjectManagement/Controllers/ProjectController.cs
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -9,9 +9,11 @@ namespace ProjectManagement.Controllers
     public class ProjectController : ControllerBase
     {
         private List<Project> _projectList;
+        private ILogger<ProjectController> _logger;
 
-        public ProjectController()
+        public ProjectController(ILogger<ProjectController> logger)
         {
+            _logger = logger;
             _projectList = new List<Project>
             {
                 new Project {Id = Guid.NewGuid(), Name = "Project 1" },
@@ -20,10 +22,26 @@ namespace ProjectManagement.Controllers
             };
         }
 
+
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok(_projectList);
+            try
+            {
+                int c = 10;
+                int a = 0;
+                int d = c / a;
+
+                _logger.LogInformation("Projects.Get() has been run.");
+                return Ok(_projectList);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Project.Get() has been crashed : " + ex.Message);
+                throw;
+            }
+
+
         }
     }
 }
