43c63a5a8b4873b1749a1de5b279407dd90435ab
Yunus <yunusinne@gmail.com>
2022-12-07 00:46:03 +0300

    Added: +RepositoryBase

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+    {
+
+    }
+}
