ae2a1f0542900a2a4f9fd0578aa3dbf4289b6554
Yunus <yunusinne@gmail.com>
2022-12-06 23:52:38 +0300

    Added: +Repository Context

--- Entities/Models/Project.cs
+++ Entities/Models/Project.cs
@@ -1,9 +1,19 @@
-﻿namespace Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
 {
     public class Project
     {
+        [Column("ProjectId")]
         public Guid Id { get; set; }
-        public string Name { get; set; }
-
+        [Required(ErrorMessage = "Project name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length of the name is 60 characters.")]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string Field { get; set; }
+        public string? ImageUrl { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
     }
+
 }
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext :DbContext
+    {
+        public RepositoryContext(DbContextOptions options): base(options) 
+        {
+
+        }
+
+        DbSet<Project> Projects { get; set; }
+        DbSet<Employee> Employees { get; set; }
+
+    }
+}
