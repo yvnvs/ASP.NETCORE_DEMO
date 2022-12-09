7d8b4359739a4c824fcdfc15ad647dbbb00e68b8
Yunus <yunusinne@gmail.com>
2022-12-09 21:44:55 +0300

    Lazy Loading for RepositoryManager and ServiceManager

--- ProjectManagement/Controllers/ProjectController.cs
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -1,5 +1,4 @@
-﻿using Contracts;
-using Entities.Models;
+﻿using Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Repository;
@@ -9,25 +8,20 @@ namespace ProjectManagement.Controllers
 {
     [Route("api/projects")]
     [ApiController]
-    public class ProjectController : ControllerBase
+    public class ProjectsController : ControllerBase
     {
-
         private IServiceManager _service;
 
-        public ProjectController(IServiceManager service)
+        public ProjectsController(IServiceManager service)
         {
             _service = service;
-           
         }
-
 
         [HttpGet]
         public IActionResult Get()
         {
-            
-
             var list = _service.ProjectService.GetAllProjects(false);
-                return Ok(list);
+            return Ok(list);
         }
     }
 }
