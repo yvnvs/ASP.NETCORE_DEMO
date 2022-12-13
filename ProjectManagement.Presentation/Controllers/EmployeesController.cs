10c57f8c96b32be410c4feab050453239e044711
Yunus <yunusinne@gmail.com>
2022-12-13 18:47:47 +0300

    Refactor: + Error Handlig

--- ProjectManagement.Presentation/Controllers/EmployeesController.cs
+++ ProjectManagement.Presentation/Controllers/EmployeesController.cs
@@ -21,36 +21,24 @@ namespace ProjectManagement.Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllEmployeeByProjectId(Guid projectId)
         {
-            try
-            {
-                var employeeList = _service
-                    .EmployeeService
-                    .GetAllEmployeesByProjectId(projectId, false);
 
-                return Ok(employeeList);
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Internal Server Error!");
-            }
+            var employeeList = _service
+                .EmployeeService
+                .GetAllEmployeesByProjectId(projectId, false);
+
+            return Ok(employeeList);
+
         }
 
         [HttpGet("{id:guid}")]
         public IActionResult GetOneEmployeeByProjectId(Guid projectId, Guid id)
         {
-            try
-            {
-                var employee = _service
-                    .EmployeeService
-                    .GetOneEmployeeByProjectId(projectId, id, false);
 
-                return Ok(employee);
-            }
-            catch
-            {
-                return StatusCode(500, "Internal server error!");
-            }
+            var employee = _service
+                .EmployeeService
+                .GetOneEmployeeByProjectId(projectId, id, false);
+
+            return Ok(employee);
         }
-
     }
-} 
+}
